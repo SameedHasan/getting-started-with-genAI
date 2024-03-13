2bd6433822b4f9d3eb0c7b8c450ca8ae881b5581
Sameed Hasan <sameedh41@gmail.com>
2024-03-13 18:43:30 +0500

    task 30 done

---
+++ 30.Hello Admin/app.ts
@@ -0,0 +1,18 @@
+//  Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+const users: string[] = ["admin", "sameed", "hasan", "saad", "mustafa"];
+
+users.forEach((user) =>
+  console.log(`Hello ${user}, thank you for logging in again`)
+);
+
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+users.forEach((user) =>
+  console.log(
+    `Hello ${user}, ${
+      user === "admin"
+        ? "would you like to see a status report?"
+        : "thank you for logging in again"
+    }`
+  )
+);
