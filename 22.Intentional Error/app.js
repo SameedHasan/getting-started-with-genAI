4e7b09e82331acf8942c27d44133bbe378985100
Sameed Hasan <sameedh41@gmail.com>
2024-03-13 16:32:21 +0500

    task 22 done

---
+++ 22.Intentional Error/app.js
@@ -0,0 +1,6 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+var languages = ["Urdu", "English", "French", "Chinese", "Hindi"];
+var hindi = languages[7];
+console.log("Hindi :>> ", hindi);
+hindi = languages[4];
+console.log("Hindi :>> ", hindi);
---
+++ 22.Intentional Error/app.ts
@@ -0,0 +1,8 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let languages: string[] = ["Urdu", "English", "French", "Chinese", "Hindi"];
+
+let hindi: string = languages[7];
+
+console.log("Hindi :>> ", hindi);
+hindi = languages[4];
+console.log("Hindi :>> ", hindi);
