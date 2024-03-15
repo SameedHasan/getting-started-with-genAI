6181c679fad3aeec49b1155ea18aa15f5d7dbfde
Sameed Hasan <sameedh41@gmail.com>
2024-03-16 01:00:27 +0500

    task 42 done

---
+++ 42.Great Magicians/app.js
@@ -0,0 +1,15 @@
+// Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) { return console.log(magician); });
+}
+function make_great(magicians) {
+    var great_magicians = magicians.map(function (magician) { return "The Great " + magician; });
+    show_magicians(great_magicians);
+}
+var magicians = [
+    "Harry Houdini",
+    "David Blaine",
+    "Doug Henning",
+    "Dai Vernon",
+];
+make_great(magicians);
